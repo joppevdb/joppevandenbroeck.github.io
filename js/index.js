01f94f1c29f5b7746b88302cf717a9387547fbda
joppevandenbroeck <71765408+joppevdb@users.noreply.github.com>
2022-11-15 17:17:27 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 const btnAlle = document.getElementById("alle")
+const btnSoort = document.getElementById("soort")
 
-const overzicht = document.getElementById("overzicht")
+const overzichtAlle = document.getElementById("overzichtAlle")
+const overzichtSoort = document.getElementById("overzichtSoort")
 
 btnAlle.addEventListener('click', function (e){
-    overzicht.classList.add("visible")
-    overzicht.classList.remove("invisible")
+    overzichtAlle.classList.add("visible")
+    overzichtSoort.classList.add("invisible")
+    overzichtSoort.classList.remove("visible")
+    overzichtAlle.classList.remove("invisible")
 })
+
+btnSoort.addEventListener('click', function (e){
+    overzichtAlle.classList.add("invisible")
+    overzichtSoort.classList.add("visible")
+    overzichtSoort.classList.remove("invisible")
+    overzichtAlle.classList.remove("visible")
+})
+
