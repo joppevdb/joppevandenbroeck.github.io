44439ee13572b473ad7addb80294329c365cdca3
Joppe Van den Broeck <r0842949@student.thomasmore.be>
2022-11-15 21:58:34 +0100

    alle bieren

--- js/index.js
+++ js/index.js
@@ -1,20 +1,40 @@
 const btnAlle = document.getElementById("alle")
 const btnSoort = document.getElementById("soort")
+const btnBrouwerij = document.getElementById("brouwerij")
 
 const overzichtAlle = document.getElementById("overzichtAlle")
 const overzichtSoort = document.getElementById("overzichtSoort")
+const overzichtBrouwerij = document.getElementById("overzichtBrouwerij")
 
 btnAlle.addEventListener('click', function (e){
     overzichtAlle.classList.add("visible")
     overzichtSoort.classList.add("invisible")
-    overzichtSoort.classList.remove("visible")
+    overzichtBrouwerij.classList.add("invisible")
+
     overzichtAlle.classList.remove("invisible")
+    overzichtSoort.classList.remove("visible")
+    overzichtBrouwerij.classList.remove("visible")
+
 })
 
 btnSoort.addEventListener('click', function (e){
     overzichtAlle.classList.add("invisible")
     overzichtSoort.classList.add("visible")
-    overzichtSoort.classList.remove("invisible")
+    overzichtBrouwerij.classList.add("invisible")
+
     overzichtAlle.classList.remove("visible")
+    overzichtSoort.classList.remove("invisible")
+    overzichtBrouwerij.classList.remove("visible")
+
+})
+
+btnSoort.addEventListener('click', function (e){
+    overzichtAlle.classList.add("invisible")
+    overzichtSoort.classList.add("invisible")
+    overzichtBrouwerij.classList.add("visible")
+
+    overzichtAlle.classList.remove("visible")
+    overzichtSoort.classList.remove("visible")
+    overzichtBrouwerij.classList.remove("invisible")
 })
 
