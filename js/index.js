fb61433952acd55317e63837bc0062ada099b6ba
Joppe Van den Broeck <r0842949@student.thomasmore.be>
2022-11-16 11:06:24 +0100

    alle bieren

--- js/index.js
+++ js/index.js
@@ -1,40 +1,46 @@
 const btnAlle = document.getElementById("alle")
 const btnSoort = document.getElementById("soort")
 const btnBrouwerij = document.getElementById("brouwerij")
+const btnZoeken = document.getElementById("zoeken")
 
 const overzichtAlle = document.getElementById("overzichtAlle")
 const overzichtSoort = document.getElementById("overzichtSoort")
+const zoekbalk = document.getElementById("zoekbalk")
 const overzichtBrouwerij = document.getElementById("overzichtBrouwerij")
 
 btnAlle.addEventListener('click', function (e){
     overzichtAlle.classList.add("visible")
     overzichtSoort.classList.add("invisible")
-    overzichtBrouwerij.classList.add("invisible")
+    zoekbalk.classList.add("invisible")
 
     overzichtAlle.classList.remove("invisible")
     overzichtSoort.classList.remove("visible")
-    overzichtBrouwerij.classList.remove("visible")
+    zoekbalk.classList.remove("visible")
 
 })
 
 btnSoort.addEventListener('click', function (e){
     overzichtAlle.classList.add("invisible")
     overzichtSoort.classList.add("visible")
-    overzichtBrouwerij.classList.add("invisible")
+    zoekbalk.classList.add("invisible")
 
     overzichtAlle.classList.remove("visible")
     overzichtSoort.classList.remove("invisible")
-    overzichtBrouwerij.classList.remove("visible")
+    zoekbalk.classList.remove("visible")
 
 })
 
 btnBrouwerij.addEventListener('click', function (e){
     overzichtAlle.classList.add("invisible")
     overzichtSoort.classList.add("invisible")
-    overzichtBrouwerij.classList.add("visible")
+    zoekbalk.classList.add("visible")
 
     overzichtAlle.classList.remove("visible")
     overzichtSoort.classList.remove("visible")
-    overzichtBrouwerij.classList.remove("invisible")
+    zoekbalk.classList.remove("invisible")
+})
+
+btnZoeken.addEventListener('click', function (e){
+    overzichtBrouwerij.classList.add("visible")
 })
 
